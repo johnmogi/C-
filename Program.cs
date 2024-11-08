0c0520e82b9e16f82994aa21654608fbf75f978c
johnmogi <anguru@gmail.com>
2024-11-08 20:06:43 +0100

    start

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// using System.Runtime.CompilerServices;
+
+// Console.WriteLine("Hello, World!");
+
+// string songLyrics = "You say goodbye, and I say hello";
+
+// Console.WriteLine($"{songLyrics.Contains("goodbye")}");
+
+// string johnName = "John Klein";
+
+// string newJohn = johnName.Replace("John Klein", "John Mogi");
+// // Console.WriteLine(newJohn);
+// Console.WriteLine(newJohn.StartsWith("Jo"));
+
+// int - natural number
+// double - common number
+// float -  1.3M natural number
+
+int a = 4;
+int b = 5;
+int c = a + b;
+Console.WriteLine(c);
